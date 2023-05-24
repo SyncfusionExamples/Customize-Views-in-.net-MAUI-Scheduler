3b209039b9dfd21f35e341d717c64e958d47e837
Surya <surya.elayaperumal@syncfusion.com>
2023-05-24 13:02:48 +0530

    Add customization codes

--- Maui_Scheduler/Maui_Scheduler/MainPage.xaml.cs
+++ Maui_Scheduler/Maui_Scheduler/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Syncfusion.Maui.Scheduler;
+using System.Collections.ObjectModel;
 
 namespace Maui_Scheduler;
 
@@ -6,8 +7,22 @@ public partial class MainPage : ContentPage
 {
     public MainPage()
     {
+
         InitializeComponent();
-        
+        var timeRegions = new ObservableCollection<SchedulerTimeRegion>();
+        var timeRegion = new SchedulerTimeRegion()
+        {
+            StartTime = DateTime.Today.AddHours(13),
+            EndTime = DateTime.Today.AddHours(14),
+            Text = "Lunch",
+            EnablePointerInteraction = false,
+            RecurrenceRule = "FERQ:DAILY,INTERVAL=1"
+        };
+
+        timeRegions.Add(timeRegion);
+        this.Scheduler.DaysView.TimeRegions = timeRegions;
+        this.Scheduler.TimelineView.TimeRegions = timeRegions;
+
     }
 }
 
